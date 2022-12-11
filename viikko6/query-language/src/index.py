2c0ed049c65a50898166e5b4c1be91fcce54308b
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2022-12-11 23:17:37 +0200

    Or class added

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
@@ -27,10 +27,19 @@ def main():
     #for player in stats.matches(matcher):
         #print(player)
 
+    #matcher = And(
+    #HasFewerThan(1, "goals"),
+    #PlaysIn("NYR")
+    #)
+
     matcher = And(
-    HasFewerThan(1, "goals"),
-    PlaysIn("NYR")
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
     )
+)
 
     for player in stats.matches(matcher):
         print(player)
