7effda7b62a4f75fa23934c1bec7f92347031d3d
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2022-12-21 17:08:05 +0200

    pääohjelmalta poistettu konkreettiset riippuvuudet pelin toteuttaviin luokkiin

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,7 +1,3 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
-from kivipaperisakset import KiviPaperiSakset
 from luo_peli import LuoPeli
 
 
@@ -21,33 +17,6 @@ def main():
             break
         peli.pelaa()
 
-"""
-
-
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
-        else:
-            break
-"""
 
 if __name__ == "__main__":
     main()
